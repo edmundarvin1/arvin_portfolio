3ba518876c6314a3399dbf7a3e31765e6b7557e9
arvin-deleon <arvin.deleon@speqta.com>
2025-03-01 17:35:35 +0100

    Change port

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import http.server
 import socketserver
 import os
 
-PORT = 8080  # Standard alternative HTTP port
+PORT = 8081
 DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 
 class CORSHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
